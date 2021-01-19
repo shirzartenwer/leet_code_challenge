469be283267e87304eb46f4390e5ac7ad6b17cbb
Shirzart Enwer <shirzartenwer@gmail.com>
2021-01-19 15:13:18 +0100

    feat: Just started with recursive functions

---
+++ Fibonacci_number/solution_recursive_function.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n <=1:
+            return n 
+        else:
+            return (self.fib(n-1) + self.fib(n-2))
+            
+        
