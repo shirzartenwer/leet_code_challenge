4d5bfdfa8c0aa16f93bb8a02d506d9ac2b2ebe50
Shirzart Enwer <shirzartenwer@gmail.com>
2021-04-12 23:15:39 +0200

    feat: created parking system

---
+++ design_a_parking_system/DesignParkingSystem.py
@@ -0,0 +1,17 @@
+
+
+# https://leetcode.com/problems/design-parking-system/
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        pass
+        
+
+    def addCar(self, carType: int) -> bool:
+        pass
+        
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
