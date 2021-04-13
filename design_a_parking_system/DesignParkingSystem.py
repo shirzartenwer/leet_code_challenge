f17bd69c7554344769d4c4b182bbfbbc02400afe
Shirzart Enwer <shirzartenwer@gmail.com>
2021-04-13 23:24:27 +0200

    feat: car parking system solution one

--- design_a_parking_system/DesignParkingSystem.py
+++ design_a_parking_system/DesignParkingSystem.py
@@ -1,15 +1,40 @@
 
 
 # https://leetcode.com/problems/design-parking-system/
-class ParkingSystem:
+class ParkingSystem(object):
 
-    def __init__(self, big: int, medium: int, small: int):
-        pass
+    def __init__(self, big, medium, small):
+        """
+        :type big: int
+        :type medium: int
+        :type small: int
+        """
+        
+        self.big = big
+        self.medium = medium
+        self.small = small
         
 
-    def addCar(self, carType: int) -> bool:
-        pass
+    def addCar(self, carType):
+        """
+        :type carType: int
+        :rtype: bool
+        """
+        if carType is 1:
+            if self.big > 0:
+                self.big -= 1
+                return True
+        elif carType is 2:
+            if self.medium > 0:
+                self.medium -= 1
+                return True
+        elif carType is 3:
+            if self.small > 0:
+                self.small -= 1
+                return True
         
+        return False
+
 
 
 # Your ParkingSystem object will be instantiated and called as such:
