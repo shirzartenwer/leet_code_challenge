5b61e684067f64e07f0b3c6b6143b55259a5f524
Shirzart Enwer <shirzartenwer@gmail.com>
2021-04-14 23:48:29 +0200

    Create subrectangle_queries.py

---
+++ subrectangle_queries/subrectangle_queries.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/subrectangle-queries/
+class SubrectangleQueries:
+
+    def __init__(self, rectangle: List[List[int]]):
+        pass
+        
+
+    def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+        pass
+        
+
+    def getValue(self, row: int, col: int) -> int:
+        pass
+        
+
+
+# Your SubrectangleQueries object will be instantiated and called as such:
+# obj = SubrectangleQueries(rectangle)
+# obj.updateSubrectangle(row1,col1,row2,col2,newValue)
+# param_2 = obj.getValue(row,col)
