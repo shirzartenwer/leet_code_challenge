de3e9c7b43fe3aa94210505e9c48eb9611fe418a
Shirzart Enwer <shirzartenwer@gmail.com>
2021-04-22 23:19:43 +0200

    feat: included bucket sort solutions and heap

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # leet code challenge
 
-Documentaing my progress on leetcode challenges. It also serves for the purpose of optimising my solution.
+Documenting my progress on leetcode challenges. It also serves for the purpose of optimising my solution.
+
+
+## Discovered during coding
+
+```python
+count = Counter(nums)  
+```
+
+If a `nums` is a list, count is a dictionary ordered by the decreasing frequency of the list member.
+The key would be the list element, the value will be the frequency.
--- Sorting/TopKFrequentElement/top_k_frequent_element.py
+++ Sorting/TopKFrequentElement/top_k_frequent_element.py
@@ -1,5 +1,6 @@
 from typing import *
-
+from collections import Counter
+import heapq
 #TODO: why this works, but not my sorting work
 
 class Solution:
@@ -13,7 +14,33 @@ class Solution:
                 
         return list(sorted(dict, key=dict.get, reverse=True))[0:k]
     
+ 
+# Solution from leetcode 
+
+
+class Solution_2:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]: 
+        # O(1) time 
+        if k == len(nums):
+            return nums
+        
+        # 1. build hash map : character and how often it appears
+        # O(N) time
+        count = Counter(nums)   
+        # 2-3. build heap of top k frequent elements and
+        # convert it into an output array
+        # O(N log k) time
+        return heapq.nlargest(k, count.keys(), key=count.get)     
     
+
+# bucket sort idea from leetcode discussion
+class BucketSort:
+    def topKFrequent(self, nums, k):
+        bucket = [[] for _ in range(len(nums) + 1)]
+        Count = Counter(nums).items()  
+        for num, freq in Count: bucket[freq].append(num) 
+        flat_list = [item for sublist in bucket for item in sublist]
+        return flat_list[::-1][:k]
 
 # class MySolution:
 #     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
