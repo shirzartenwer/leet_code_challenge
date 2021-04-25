283b03b03a2768fe04bfa7b27908210646b70889
Shirzart Enwer <shirzartenwer@gmail.com>
2021-04-25 22:37:46 +0200

    feat: included my solution which need to be debugged

--- maximum_ice_cream_bars/maximum_ice_cream_bars.py
+++ maximum_ice_cream_bars/maximum_ice_cream_bars.py
@@ -1,3 +1,4 @@
+from typing import *
 # https://leetcode.com/problems/maximum-ice-cream-bars/discuss/1174585/Python-sort
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
@@ -12,3 +13,17 @@ class Solution:
                 break
             
         return count
+    
+    
+    
+#TODO: check out why this solution didn't work
+class MySolution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        for k in range(len(costs)):
+            if costs[0] > coins:
+                return 0
+            elif sum(costs[:k]) >= coins:
+                return k 
+            elif sum(costs) <= coins:
+                return len(costs)
