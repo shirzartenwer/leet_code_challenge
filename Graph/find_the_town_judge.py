68869a8ff2e36e10da92023fb186154ae433a5e9
Shirzart Enwer <shirzartenwer@gmail.com>
2021-06-15 23:24:16 +0200

    feat: include a solution from the forum

--- Graph/find_the_town_judge.py
+++ Graph/find_the_town_judge.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/find-the-town-judge/
 from collections import Counter
 from typing import List
 
@@ -12,3 +13,22 @@ class Solution:
             pass
         else:
             return -1
+        
+        
+# class SolutionFromForum:
+#     def findJudge(self, n: int, trust: List[List[int]]) -> int:
+#         first = []
+#         second = []
+#         if n==1 and len(trust)==0:
+#             return 1
+#         for i in trust:
+#             first.append(i[0])
+#             second.append(i[1])
+#         x = list((set(second)-set(first)))
+#         if len(x)!=0:
+#             x = x[0]
+#         else:
+#             return -1
+#         if second.count(x)<n-1:
+#             return -1
+#         return x
