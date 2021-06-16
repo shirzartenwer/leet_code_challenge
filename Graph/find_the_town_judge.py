2149311ccc92f028758e26fde7dff724a6d9ec79
Shirzart Enwer <shirzartenwer@gmail.com>
2021-06-16 23:29:30 +0200

    feat: review solution

--- Graph/find_the_town_judge.py
+++ Graph/find_the_town_judge.py
@@ -15,20 +15,20 @@ class Solution:
             return -1
         
         
-# class SolutionFromForum:
-#     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-#         first = []
-#         second = []
-#         if n==1 and len(trust)==0:
-#             return 1
-#         for i in trust:
-#             first.append(i[0])
-#             second.append(i[1])
-#         x = list((set(second)-set(first)))
-#         if len(x)!=0:
-#             x = x[0]
-#         else:
-#             return -1
-#         if second.count(x)<n-1:
-#             return -1
-#         return x
+class SolutionFromForum:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        first = []
+        second = []
+        if n==1 and len(trust)==0:
+            return 1
+        for i in trust:
+            first.append(i[0])
+            second.append(i[1])
+        x = list((set(second)-set(first)))
+        if len(x)!=0:
+            x = x[0]
+        else:
+            return -1
+        if second.count(x)<n-1:
+            return -1
+        return x
