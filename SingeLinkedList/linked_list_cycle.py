3969859a466fa966de6322c5739209ec4e99b932
Shirzart Enwer <shirzartenwer@gmail.com>
2022-04-05 23:04:39 +0200

    feat: include several methods for linked list

--- SingeLinkedList/linked_list_cycle.py
+++ SingeLinkedList/linked_list_cycle.py
@@ -1,12 +1,38 @@
 # TODO: https://leetcode.com/problems/linked-list-cycle/
 # Definition for singly-linked list.
-class ListNode:
+class Node:
     def __init__(self, x):
         self.val = x
         self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def get_head(self):
+        return self.head 
+    
+    def is_empty(self):
+        if self.head is None:
+            return True
+        else:
+            return False
+        
+    def insert_at_head(self, value:float):
+        if self.is_empty():
+            self.head = Node(value)
+        else:
+            temp_node = Node(value)
+            temp_node.next = self.head
+            self.head = temp_node
+        return self
+    
+    # TODO: implement insert at tail method
+        
+        
 
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
+    def hasCycle(self, head: Node) -> bool:
         if not head:
             return False
         s_node = head
