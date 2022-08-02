5fbb4cfb364ec7936e6ab219eecfba6298a0e2d9
Shirzart Enwer <shirzartenwer@gmail.com>
2022-08-02 19:11:07 +0200

    doc: minimal example of three way of traversing a tree

--- Tree/binary_tree_inorder_traversal.py
+++ Tree/binary_tree_inorder_traversal.py
@@ -1,16 +1,18 @@
 # https://leetcode.com/problems/binary-tree-inorder-traversal/
+from typing import List
+
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def __init__(self):
         self.traversal = []
         
-    def dfs(self,node):
+    def dfs(self,node:TreeNode):
         if node.left:
             self.dfs(node.left)
         self.traversal.append(node.val)
---
+++ Tree/tree_notes.md
@@ -0,0 +1,13 @@
+# Summary of knowledge point in Tree
+
+## Tree traversial: results of different traversial 
+for the folloiwng tree:
+        1
+      /   \
+     2     3
+    / \      
+   4   5
+
+inorder: (Left, Root, Right) : 4 2 5 1 3 
+Preorder (Root, Left, Right) : 1 2 4 5 3 
+Postorder (Left, Right, Root) : 4 5 2 3 1
