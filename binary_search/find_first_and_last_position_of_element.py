06180ee43d82a40055d980ff58d2ffd18e6a6824
Shirzart Enwer <shirzartenwer@gmail.com>
2022-12-07 21:42:26 +0100

    feat: binary search solution and two pointer solution

--- binary_search/find_first_and_last_position_of_element.py
+++ binary_search/find_first_and_last_position_of_element.py
@@ -1,1 +1,34 @@
 # https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(nums) -1
+        list_index = []
+        while left <= right:
+ 
+            mid = left + (right - left) // 2
+
+            # Check if key is present at mid
+            if nums[mid] == target:
+                list_index.append(mid)
+                if nums[mid-1] == target:
+                    list_index.append(mid-1)
+                if nums[mid+1] == target:
+                    list_index.append(mid+1)
+                return sorted(list_index)
+
+            # If key is greater, ignore left half
+            elif nums[mid] < target:
+                left = mid + 1
+
+            # If key is smaller, ignore right half
+            else:
+                right = mid - 1
+
+        return [-1, 1]
+
+
+Solution().searchRange([5,7,7,8,8,10], 8)
