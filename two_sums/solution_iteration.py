ac4153e39677cea0eae8428d671c6e5c1ac3ac50
Shirzart Enwer <shirzartenwer@gmail.com>
2024-11-17 16:08:05 +0100

    feat: two sum

--- two_sums/solution_iteration.py
+++ two_sums/solution_iteration.py
@@ -1,6 +1,7 @@
 from typing import List
 import copy
 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for element_1 in nums:
@@ -9,14 +10,15 @@ class Solution:
                     return [nums.index(element_1), index_element_2]
                 else:
                     continue
-                
-            
+
+
 # trying list comprehension:
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for element_1 in nums:
-            element_2_list = [x for x in range(nums.index(element_1)+1, len(nums)) if nums[x] + element_1 == target]
+            element_2_list = [x for x in range(nums.index(
+                element_1)+1, len(nums)) if nums[x] + element_1 == target]
             if len(element_2_list) > 0:
                 return [nums.index(element_1), element_2_list[0]]
             else:
@@ -28,22 +30,22 @@ class Solution:
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # The following line gets the index of the original list sorted (https://stackoverflow.com/questions/7851077/how-to-return-index-of-a-sorted-list)
-        get_the_sorted_index= sorted(range(len(nums)), key=lambda k: nums[k])
+        get_the_sorted_index = sorted(range(len(nums)), key=lambda k: nums[k])
         nums.sort()
         left_pointer = 0
-        right_pointer = len(nums) -1
+        right_pointer = len(nums) - 1
         while left_pointer <= right_pointer:
             result = nums[left_pointer] + nums[right_pointer]
             if result < target:
-                left_pointer +=1
+                left_pointer += 1
                 continue
             elif result > target:
-                right_pointer -=1
+                right_pointer -= 1
                 continue
             else:
                 return get_the_sorted_index[left_pointer], get_the_sorted_index[right_pointer]
-            
-        
+
+
 # Some other solution:
 
 class Solution:
