8333a2add250e503243216403d5305aa03a52ba7
Shirzart Enwer <shirzartenwer@gmail.com>
2025-04-29 11:05:39 +0200

    feat: most classic sliding window problem

--- SlidingWindow/longestSubstringWithoutRepeatingCharacter.py
+++ SlidingWindow/longestSubstringWithoutRepeatingCharacter.py
@@ -25,3 +25,26 @@ class Solution:
         heapq.heappush(collection, (len(cur_char), cur_char))
 
         return heapq.nlargest(1, collection)[0][0]
+
+
+# actually an easier way is to do sliding windows
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "":
+            return 0
+
+        char_set = set()
+        l = 0
+
+        for r in range(len(s)):
+            while s[r] in char_set:
+                # they key trick is to start shrinking the string from left,
+                # becaues you want to maximize the chance of reusing other strings
+                # that are not repeated to have a longer substring in the end
+                char_set.remove(s[l])
+                l += 1
+            char_set.add(s[r])
+            res = max(res, r-l+1)
+        return res
